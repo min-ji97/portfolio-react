3d707d08b567475542d75e0ef8dcaa22546e84c4
jominji <whalswl576@gmail.com>
2024-07-14 04:50:49 +0900

    ( css ):Main.js-project + Main.js-skill & project component

--- src/Main.js
+++ src/Main.js
@@ -6,57 +6,84 @@ import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHtml5 , faCss3Alt, faSass , faJs ,faVuejs, faReact , 
   faNode , faFigma, faGithub
 } from "@fortawesome/free-brands-svg-icons";
-import { faR } from '@fortawesome/free-solid-svg-icons';
+// import { faR } from '@fortawesome/free-solid-svg-icons'; 아직 안씀!
 
 
-//<FontAwesomeIcon icon="fa-brands fa-css3-alt" /> <FontAwesomeIcon icon="fa-brands fa-github-alt" />
 
-function Main() {
+const PjComponent = ({listData}) =>{
+  return (
+    <div className={listData.className}>
+      <img src={listData.img} alt="" />
+      <span>{listData.pjName}</span>
+    </div>
+  )
+};
 
+const SkillComponent = ({listData}) => {
   const iconMap = {
+    //front
     faHtml5: faHtml5,
     faCss3Alt: faCss3Alt,
     faSass: faSass,
     faJs : faJs,
     faVuejs: faVuejs,
     faReact: faReact,
+
+    //back
     faNode: faNode,
 
+    //etc..
     faGithub:faGithub,
     faFigma: faFigma,
   }
+  return(
+    <span>
+      { listData.icon ? <FontAwesomeIcon icon={iconMap[listData.icon]} /> : null}
+      {listData.value}
+    </span>
+  )
+};
+
+function Main() {
 
   const skillFront = [
-    { id: 1 , value: 'HTML5', icon: 'faHtml5' },
-    { id: 2 , value: 'CSS3', icon: 'faCss3Alt' },
-    { id: 3 , value: 'SCSS' , icon: 'faSass' },
-    { id: 4 , value: 'JavaScript' , icon: 'faJs' },
-    { id: 5 , value: 'Jquery' , icon: '' },
-    { id: 6 , value: 'Vue.js', icon: 'faVuejs' },
-    { id: 7 , value: 'React.js' , icon: 'faReact'},
+    { value: 'HTML5', icon: 'faHtml5' },
+    { value: 'CSS3', icon: 'faCss3Alt' },
+    { value: 'SCSS' , icon: 'faSass' },
+    { value: 'JavaScript' , icon: 'faJs' },
+    { value: 'Jquery' , icon: '' },
+    { value: 'Vue.js', icon: 'faVuejs' },
+    { value: 'React.js' , icon: 'faReact'},
   
   ];
   const skillBack = [
-    { id: 1 , value: 'node.js', icon: 'faNode' },
-    { id: 2 , value: 'express.js', icon: '' },
-    { id: 3 , value: 'MySql' , icon: '' },
-    { id: 4 , value: 'MariaDb' , icon: '' },
+    { value: 'node.js', icon: 'faNode' },
+    { value: 'express.js', icon: '' },
+    { value: 'MySQL' , icon: '' },
+    { value: 'MariaDB' , icon: '' },
   ];
   const skillEtc = [
-    { id: 1 , value: 'Git', icon: 'faGithub' },
-    { id: 2, value: 'Figma', icon : 'faFigma'}
+    { value: 'Git', icon: 'faGithub' },
+    { value: 'Figma', icon : 'faFigma'}
   ];
 
-  const skillFrontList =  skillFront.map((v)=> <span key={v.id}><FontAwesomeIcon icon={iconMap[v.icon]} />{v.value}</span> );
-  const skillBackList = skillBack.map((v)=> <span key={v.id}><FontAwesomeIcon icon={iconMap[v.icon]} />{v.value}</span> );
-  const skillEtcList = skillEtc.map((v)=> <span key={v.id}><FontAwesomeIcon icon={iconMap[v.icon]} />{v.value}</span> );
+
+  const projectLists = [
+    { img : '', pjName: 'meta bus game..?' , className: 'project-content--react'},
+    { img : '', pjName: 'fruit merge game..?' , className: 'project-content--react'},
+    { img : '', pjName: 'Momentum' , className: 'project-content--js'},
+    { img : '', pjName: 'portfolio-react' , className: 'project-content--react'},
+    { img : '', pjName: '네이버 클론코딩' , className: 'project-content--js'},
+    { img : '', pjName: '스타벅스 클론코딩' , className: 'project-content--js'},
+    { img : '', pjName: 'CRUD게시판 Pet Talk!' , className: 'project-content--vue'},
+    { img : '', pjName: '졸업작품 : 누리다' , className: 'project-content--vue'},
+  ]
+
 
   return (
     <div className='main-container'>
 
-
       <header>minji's portfolio</header>
-
 
       <section id='introduce'>
         <div className='about-me'>
@@ -77,19 +104,19 @@ function Main() {
           <div className='skill-front'>
             <div>Front-End</div>
             <div>
-              {skillFrontList}
+              { skillFront.map( lists => <SkillComponent listData={lists}/>) }
             </div>
           </div>
           <div className='skill-back'>
             <div>Back-End</div>
             <div>
-              {skillBackList}
+              { skillBack.map( lists => <SkillComponent listData={lists}/>)}
             </div>
           </div>
           <div className='skill-etc'>
             <div>etc..</div>
             <div>
-              {skillEtcList}
+              { skillEtc.map( lists => <SkillComponent listData={lists}/>)}
             </div> 
           </div>
         </div>
@@ -97,7 +124,7 @@ function Main() {
 
 
       <section id='project'>
-        <div>PROJECT</div>
+        <h1>PROJECT</h1>
         <div>
           <div className='project-nav'>
             <button>All</button>
@@ -106,38 +133,7 @@ function Main() {
             <button>JavaScript</button>
           </div>
           <div className='project-content'>
-
-            <div className='project-content--react'>
-              <img src="" alt="" />
-              <span>meta bus game</span>
-            </div>
-
-            <div className='project-content--react'>
-              <img src="" alt="" />
-              <span>fruit merge game..?</span>
-            </div>
-
-            <div className='project-content--js'>
-              <img src="" alt="" />
-              <span>Momentum</span>
-            </div>
-            <div className='project-content--js'>
-              <img src="" alt="" />
-              <span>네이버 클론코딩</span>
-            </div>
-            <div className='project-content--js'>
-              <img src="" alt="" />
-              <span>스타벅스 클론코딩</span>
-            </div>
-            <div className='project-content--vue'>
-              <img src="" alt="" />
-              <span>CRUD게시판 Pet Talk!</span>
-            </div>
-            <div className='project-content--vue'>
-              <img src="" alt="" />
-              <span>졸업작품 : 누리다</span>
-            </div>
-
+           {  projectLists.map( lists => <PjComponent listData= {lists}  />) }
           </div>
         </div>
       </section>
